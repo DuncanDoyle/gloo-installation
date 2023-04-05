7cda7f7e633db9523c32b9462d8c43ba0b993245
Duncan Doyle <Duncan.Doyle@gmail.com>
2023-04-05 12:26:50 +0200

    Separated OLM installation out to its own dir.

---
+++ olm/01-deploy-olm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# This script requires the operator-sdk installed on your system. On macOS, you can use Home Brew to install the utility: brew install operator-sdk
+# 
+
+operator-sdk olm install
+
