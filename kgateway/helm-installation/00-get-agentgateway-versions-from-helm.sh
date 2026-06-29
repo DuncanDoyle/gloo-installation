aac0bc45087656ff276cbf4acf73b26fc0533227
Duncan Doyle <Duncan.Doyle@gmail.com>
2026-06-29 10:45:29 +0200

    Added kgateway.

---
+++ kgateway/helm-installation/00-get-agentgateway-versions-from-helm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This doesn't work on OCI repositories
+# helm search repo oci://ghcr.io/solo-io/helm-charts/gloo-gateway
+
+export URL="ghcr.io/kgateway-dev/charts/agentgateway"
+
+json=$(skopeo list-tags docker://$URL)
+tags=$(echo $json | jq -r '.Tags.[]' | grep -v '^sha256-')
+printf "\nTags:\n$tags\n"
+
+# for row in $tags; do
+#     skopeo inspect --config --raw docker://ghcr.io/solo-io/helm-charts/gloo-gateway:$row | jq -r
+# done
+
+latest_tag=$(echo "$tags" | tail -n1)
+
+printf "\nLatest tag: $latest_tag\n"
+
+skopeo inspect --config --raw docker://$URL:$latest_tag | jq -r
---
+++ kgateway/helm-installation/helm-pull-kgateway.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [[ -z "$1" ]]; then
+    echo "Please specify the version number of the kgateway helm chart you want to pull."
+    exit 1
+fi
+
+URL=cr.kgateway.dev/kgateway-dev/charts/kgateway
+
+helm pull oci://$URL --version $1
